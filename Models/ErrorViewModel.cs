0187b6250a5c95715985d1af63cb0e50928f6d72
Kamil <k.gorkowy@op.pl>
2025-01-02 18:25:07 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KartRentalCompany.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
